888e3b9c90822ab42843b2406e1bf73c95503224
Ruslan Hassonov <ruslan.hassonov@gmail.com>
2018-05-09 17:25:39 +0200

    Finalised the DataGrid implementation in the second tab.

---
+++ Sales_Data_Control_LINQ/CustomerInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales_Data_Control_LINQ
+{
+    class CustomerInfo
+    {
+        public string CustomerName { get; set; }
+        public decimal Total { get; set; }
+    }
+}
--- Sales_Data_Control_LINQ/MainWindow.xaml.cs
+++ Sales_Data_Control_LINQ/MainWindow.xaml.cs
@@ -27,7 +27,6 @@ namespace Sales_Data_Control_LINQ
             InitializeComponent();
             var q = from o in ctx.Orders select o;
             dg_Orders.ItemsSource = q;
-
             SaleByCountry();
         }
 
@@ -50,21 +49,63 @@ namespace Sales_Data_Control_LINQ
 
         public void SaleByCountry()
         {
-            var q_SaleTotal = from c in ctx.Customers
-                              join o in ctx.Orders on c.CustomerID equals o.CustomerID
-                              join od in ctx.Order_Details on o.OrderID equals od.OrderID
-                              group c by c.Country into Country
-                              select new
+            var q_SaleTotal = from od in ctx.Order_Details
+                              group od by od.Order.ShipCountry into odByCountry
+                              orderby odByCountry.Sum(s => s.Quantity * s.UnitPrice) descending
+                              select new CountryInfo
                               {
-                                  Country,
-                                  TotalAmount = Country.Sum()
+                                  Country = odByCountry.Key,
+                                  Total = odByCountry.Sum(s => s.Quantity * s.UnitPrice)
                               };
+
             dg_SaleByCountry.ItemsSource = q_SaleTotal;
+        }
+
+        public void CustomerByCountry()
+        {
+            CountryInfo selectedCountry = dg_SaleByCountry.SelectedItem as CountryInfo;
+            var q_CustByCount = from od in ctx.Order_Details
+                                where od.Order.ShipCountry == selectedCountry.Country
+                                group od by od.Order.ShipName into odByCustName
+                                orderby odByCustName.Sum(s => s.Quantity * s.UnitPrice) descending
+                                select new CustomerInfo
+                                {
+                                    CustomerName = odByCustName.Key,
+                                    Total = odByCustName.Sum(s => s.Quantity * s.UnitPrice)
+                                };
+
+            dg_SaleByCustomerForCountry.ItemsSource = q_CustByCount;
+        }
+
+        public void OrdersByCustomer()
+        {
+            CustomerInfo selectedCustomer = dg_SaleByCustomerForCountry.SelectedItem as CustomerInfo;
+            var q_OrderByCustomer = from od in ctx.Order_Details
+                                    where od.Order.Customer.CompanyName == selectedCustomer.CustomerName
+                                    group od by od.Order into odByOrder
+                                    select new
+                                    {
+                                        OrderID = odByOrder.Key.OrderID,
+                                        OrderDate = odByOrder.Key.OrderDate,
+                                        ShippingDate = odByOrder.Key.ShippedDate,
+                                        NameSalesman = odByOrder.Key.Employee.FirstName + " " + odByOrder.Key.Employee.LastName
+                                    };
+            dg_OrdersByCustomer.ItemsSource = q_OrderByCustomer;
         }
 
         private void dg_Orders_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             LoadOrderDetail();
+        }
+
+        private void dg_SaleByCountry_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            CustomerByCountry();
+        }
+
+        private void dg_SaleByCustomerForCountry_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            OrdersByCustomer();
         }
     }
 }
